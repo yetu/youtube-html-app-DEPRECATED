22687f49369ce4280d9376c6fd9dbea9b549147e
ElliDy <elisa.hilprecht@yetu.com>
2015-02-10 15:21:41 +0100

    Run tests with npm.

--- buildClient_server.sh
+++ buildClient_server.sh
@@ -5,6 +5,9 @@ cd clients
 echo 'install npm'
 npm install
 
+echo 'test youtube_producer client'
+npm run test-youtube-producer
+
 echo 'build youtube_producer client'
 npm run build-youtube-producer
 
