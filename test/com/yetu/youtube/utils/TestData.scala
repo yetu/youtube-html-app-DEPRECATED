d3bab3ff1c54ad0f256c21ad9d18aedd0caca609
Joe Schaul <joe.schaul@gmail.com>
2015-02-16 18:34:49 +0100

    update test data with correct access token.

--- test/com/yetu/youtube/utils/TestData.scala
+++ test/com/yetu/youtube/utils/TestData.scala
@@ -1,29 +1,11 @@
 package com.yetu.youtube.utils
 
-import play.api.libs.json.{Json, JsValue}
+import play.api.libs.json.{JsValue, Json}
 
 trait TestData {
 
 
   val jsonDummyValue: JsValue = Json parse """{"source":"453672727272"}"""
 
-
-  val fakeValidJWTAccessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzUxMiJ9." +
-    "eyJzY29wZSI6ImJhciIsInVzZXJVVUlEIjoic29tZS11c2VyLXN0cmFuZ" +
-    "2UtdXVpZCIsImNsaWVudElkIjoiY29tLnlldHUudGVzdC5hcHAuaWQiLC" +
-    "JpYXQiOjE0MjI2MzU5MTIsImV4cCI6MTczMzY3NTkxMiwiYXVkIjoidGV" +
-    "zdCIsImlzcyI6Imh0dHBzOi8vYXV0aC55ZXR1ZGV2LmNvbSIsInN1YiI6I" +
-    "nN1YmplY3Qud2l0aC5kb3RzIn0.kmjhtTnOyYiFu5cMYJ-G1kaPyAspi0z" +
-    "68s1tmS-2q8RU3AXjbpSDlAdVyjJohHnVMX08oS15Rg3mZQTk_tSNft4An" +
-    "DKbpLw7x2_HXZmHofbi-9wXhiQjUp8dCdlugUOc6H04t_bOE0Pj8kE3Wg5" +
-    "Z2TGrbNoW6jpl_fBloP-ZfxJuppIhLuRyxE6jVOc57x8LOGhNtaWaQmvvK" +
-    "8GnHZa1aIOKtyHt1Qm263ZbNRgDKOCPfZOIvrNU_g7-aCYQik8oaFCiD2a" +
-    "TA7qK-VXvmYchY9tgAq2wGZ2qjmIdYczbuzDdCH-1lrYW9JGeCKA_gJnMr" +
-    "WQRbMrlHZLj0obl0GyHO30M55A4CGLo1VTd-NG2YbxhnEoO8LEOw4XvzSn" +
-    "5ISBY1ht5A7mGAx8mdh79Wz1W0WY7N-ZHRsX5G9uowR1GVaBtxpiykwE5yD" +
-    "HjmF3FzlJZB3WHjBKHi0I6Vqzl_Wt2EAK72gnlwPPag9eUgyub0Jn96z7_i" +
-    "2H7doGSNzMZp8sg-1UQeqeexk55lbk8HocZ-vwonbPtRFw8-Yr34RduS_oL" +
-    "JzthOjijjfboGVgMGEiZe8Lij9sGt8OkS690yehokto_U4Gn6MyQ_aSLsqX" +
-    "j7NPMDdAv_7VX_HKsuTJEd7_pcT1sNTqtGxwCLsRIYTeLsyepd2yeWxmhhQ" +
-    "rLKz3_cm4"
+  val fakeValidJWTAccessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzUxMiJ9.eyJ1c2VyVVVJRCI6InNvbWUtdXNlci1zdHJhbmdlLXV1aWQiLCJjbGllbnRJZCI6ImNvbS5hbm90aGVyLnRlc3QuYXBwLmlkIiwiaWF0IjoxNDI0MDkyNTkyLCJleHAiOjE3MzUxMzI1OTIsImF1ZCI6ImV2ZW50cyIsImlzcyI6Imh0dHBzOi8vYXV0aC55ZXR1ZGV2LmNvbSIsInN1YiI6InN1YmplY3Qud2l0aC5kb3RzIn0.Ia9ZYgG_V8h8c4iX0cHsL5e6wxWdsLJbI25eJNzmFa1PSsQyLMai_Q-vWXsQ83iURJsWrZxeCKih8qLKPu0z3K3A3NQV8K9qUHg8ItzaY-rsrRyF6G1eytmpsuvk5DA4fa8Yt-bkw1kChbY0Z7hzDe38a-ufwjRfTpy4Q2MZEFp_nxmKMlYF1y80yS6BysouQC1ad2PlyUrUKXBjleS1bRhmTlHBoG0S_yGGMHQ6u037Ref2yu8o0XNPWBMcqrW4UDhwZqoSWu0oPzytDHfboP_xgPnycbOQG0xK5UVplnA8n7iUK9k9PTPIs_ZHmHx73uQ_bBBbYTfhuNT2Ien1IvYXhtCW3QvNkIrnZY5stLXnpxy9FqA2WFRoN5qT40SIxC1Yd0aSpxlK34gkXWdw8JxBeYOJcusJZFm1ptUSKVoGecyeR3CxhMoGiWEf6hmQ3h4mpWwBmGEk25NT2a4d4qCheyhjsd464pNo4SmgRQ5sCt9UE5JATRRBnSAl6idYeKqAyFB89JYxpqHeOLnX-qD3unFQKgW-UdC7oFjm7R_DdKx0p_AjvEisjGpnGLHqPvMSlpprXGPLCZssCam_hYovAvdaHc7CAvJmkKqq5IFc4naN8G8ZSk_4VXkkTUIY3dJDIJMI7NcAB6HHu-1dPqsXfIV16qLJlStTWcFzKTQ"
 }
