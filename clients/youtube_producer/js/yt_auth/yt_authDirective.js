ede0196299c3e10687eb32380825d022856eeec4
Joe Schaul <joe.schaul@gmail.com>
2015-02-17 11:32:26 +0100

    formatting and renaming changes.

--- clients/youtube_producer/js/yt_auth/yt_authDirective.js
+++ clients/youtube_producer/js/yt_auth/yt_authDirective.js
@@ -1,47 +1,47 @@
 module.exports = function ($window, $http, $interval, $log) {
-	'use strict';
-	return {
-		restrict: 'E',
-		link: function(scope, element, attr){
-			//TODO: use more angular.js methods instead of mix of native and angular stuff
+    'use strict';
+    return {
+        restrict: 'E',
+        link: function (scope, element, attr) {
+            //TODO: use more angular.js methods instead of mix of native and angular stuff
 
-			var openidIframe = document.createElement('iframe');
+            var openidIframe = document.createElement('iframe');
 
-			openidIframe.src = config.authServer + '/assets/login_status.html';
-			openidIframe.id = 'openid-provider';
-			openidIframe.style.visibility = 'hidden';
-			openidIframe.style.display = 'none';
+            openidIframe.src = config.authServer + '/assets/login_status.html';
+            openidIframe.id = 'openid-provider';
+            openidIframe.style.visibility = 'hidden';
+            openidIframe.style.display = 'none';
 
-			document.body.appendChild(openidIframe);
+            document.body.appendChild(openidIframe);
 
-			openidIframe.onload = check_session;
+            openidIframe.onload = check_session;
 
-			var timerID = setInterval(check_session, config.sessionPollingInterval * 1000);
+            var timerID = setInterval(check_session, config.sessionPollingInterval * 1000);
 
-			function check_session() {
-					var win = openidIframe.contentWindow;
-					win.postMessage('youtubeApp ' + config.userUUID, config.authServer);
-			}
+            function check_session() {
+                var win = openidIframe.contentWindow;
+                win.postMessage('youtubeApp ' + config.userUUID, config.authServer);
+            }
 
-			
-			function receiveMessageP(e) {
-					if(e.originalEvent){
-						e = e.originalEvent;
-					}
-					if (e.origin !== config.authServer) {
-							$log.log('domain does not match!');
-							return;
-					}
-					var stat = e.data;
-					$log.log('poller | received message:' + stat);
-					if (stat == 'invalid') {
-						$log.log('session=invalid! Logging out and redirecting');
-						clearInterval(timerID);
-						$window.location.href ='/signOut';
-					}
-			}
 
-			angular.element($window).on('message', receiveMessageP);
-		}
-	}
+            function receiveMessageP(event) {
+                if (event.originalEvent) {
+                    event = event.originalEvent;
+                }
+                if (event.origin !== config.authServer) {
+                    $log.log('event.origin domain [' + event.origin + '] does not match the configured domain [' + config.authServer + ']');
+                    return;
+                }
+                var stat = event.data;
+                $log.log('poller | received message:' + stat);
+                if (stat == 'invalid') {
+                    $log.log('session=invalid! Logging out and redirecting');
+                    clearInterval(timerID);
+                    $window.location.href = '/signOut';
+                }
+            }
+
+            angular.element($window).on('message', receiveMessageP);
+        }
+    }
 };
